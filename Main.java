a130f4bc5a3242b2400eacb9cc96b0fda9389271
sj <64205269+shrujaya@users.noreply.github.com>
2021-07-08 16:41:47 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    JFrame frame = new JFrame();
+	    GamePlay gamePlay = new GamePlay();
+	    frame.setBounds(10, 10, 708, 600);
+	    frame.setTitle("Atari Breakout");
+	    frame.setResizable(true);
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.add(gamePlay);
+		frame.setVisible(true);
+    }
+}
